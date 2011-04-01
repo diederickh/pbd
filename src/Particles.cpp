f0b08e3952d0e3aea279125b0031669db848a868
roxlu <diederick@apollomedia.nl>
2011-04-01 22:55:33 +0200

    Quick update

--- src/Particles.cpp
+++ src/Particles.cpp
@@ -51,22 +51,33 @@ void Particles::draw() {
 // Solve
 // -----------------------------------
 void Particles::update() {
-	vector<Particle*>::iterator it = particles.begin();
-	while(it != particles.end()) {
-		Particle& particle = **it;
+	// project constraints.
+	{
+		vector<Constraint*>::iterator it = constraints.begin();
+		while(it != constraints.end()) {
+			(*it)->update();
+			++it;
+		}
+	}	
+	
+	// project position.
+	{
+		vector<Particle*>::iterator it = particles.begin();
+		while(it != particles.end()) {
+			Particle& particle = **it;
 
-		// prediction step
-		particle.velocity += particle.forces;
-		particle.predicted_position = particle.position + particle.velocity;
-		particle.forces.set(0,0,0);
-		
-		// add damping... (TODO)
-		
-		// generate constraints
-		
-		// solve constraints
-		
-		// update vertices
-		++it;
+			// prediction step
+			particle.velocity += particle.forces;
+			particle.predicted_position = particle.position + particle.velocity;
+			
+			// add displacement
+			// ---------------------------------
+			// ---------------------------------
+			
+			// clear forces.
+			particle.forces.set(0,0,0);
+			
+			++it;
+		}
 	}
 }
